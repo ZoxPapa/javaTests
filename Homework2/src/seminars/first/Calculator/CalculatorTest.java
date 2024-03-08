ac448681d64031db2cc06642de91c8b0ba585968
Zakhar <zox_13@mail.ru>
2024-03-08 07:02:18 +0300

    Homework2 calculator discount method and tests complete

---
+++ Homework2/src/seminars/first/Calculator/CalculatorTest.java
@@ -0,0 +1,98 @@
+package seminars.first.Calculator;
+
+import seminars.first.Calculator.Calculator;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        // Проверка базового функционала с целыми числами:
+        if (8 != Calculator.calculation(2, 6, '+')) {
+            throw new AssertionError("Ошибка в методе");
+        }
+
+        if (0 != Calculator.calculation(2, 2, '-')) {
+            throw new AssertionError("Ошибка в методе");
+        }
+
+        if (14 != Calculator.calculation(2, 7, '*')) {
+            throw new AssertionError("Ошибка в методе");
+        }
+
+        if (2 != Calculator.calculation(100, 50, '/')) {
+            throw new AssertionError("Ошибка в методе");
+        }
+
+        // Случаи с неправильными аргументами
+        // аргумент operator типа char, должен вызывать исключение, если он получает не базовые символы (+-*/)
+        // try {
+        //     seminars.first.Calculator.Calculator.calculation(8, 4, '_');
+        // } catch (IllegalStateException e) {
+        //     if (!e.getMessage().equals("Unexpected value operator: _")) {
+        //         throw new AssertionError("Ошибка в методе");
+        //     }
+        // }
+
+        // Проверка базового функционала с целыми числами, с использованием утверждений:
+        assert 8 == Calculator.calculation(2, 6, '+');
+        assert 0 == Calculator.calculation(2, 2, '-');
+        assert 14 == Calculator.calculation(2, 7, '*');
+        assert 2 == Calculator.calculation(100, 50, '/');
+
+        // Проверка базового функционала с целыми числами, с использованием утверждений AssertJ:
+//        assertThat(Calculator.calculation(2, 6, '+')).isEqualTo(8);
+//        assertThat(Calculator.calculation(2, 2, '-')).isEqualTo(0);
+//        assertThat(Calculator.calculation(2, 7, '*')).isEqualTo(14);
+//        assertThat(Calculator.calculation(100, 50, '/')).isEqualTo(2);
+
+        // Проверка ожидаемого исключения, с использованием утверждений AssertJ:
+//        assertThatThrownBy(() ->
+//                Calculator.calculation(8, 4, '_')
+//        ).isInstanceOf(IllegalStateException.class);
+
+//        System.out.println(Calculator.calculation(2_147_483_647, 1, '+')); // integer overflow
+//        System.out.println(Calculator.squareRootExtraction(169));
+
+        // Примерные решения домашних заданий из 1 лекции:
+
+        // HW1.1: Придумайте и опишите (можно в псевдокоде) функцию извлечения корня и
+        // необходимые проверки для него используя граничные случаи
+        // assertThatThrownBy(() ->
+        //         seminars.first.Calculator.Calculator.squareRootExtraction(0, 1, -1)
+        // ).isInstanceOf(SomeStateException.class);
+
+        // HW1.2: Как будет выглядеть проверка для случая деления на ноль? (с использованием AssertJ)
+        // assertThatThrownBy(() ->
+        //        seminars.first.Calculator.Calculator.calculation(5, 0, '/')
+        // ).isInstanceOf(ArithmeticException.class);
+
+        // HW1.3: Сравните одну и ту же проверку с использованием условий, ассертов, AssertJ
+        // в каком случае стандартное сообщение об ошибке будет более информативным?
+        // if (0 != seminars.first.Calculator.Calculator.calculation(2, 6, '+')) {
+        //     throw new AssertionError("Ошибка в методе");
+        // }
+        //   assert 0 == seminars.first.Calculator.Calculator.calculation(2, 6, '+');
+        //    assertThat(seminars.first.Calculator.Calculator.calculation(2, 6, '+')).isEqualTo(0);
+
+        //Проверка метода определения стоимости товара с учетом скидки
+        //Проверка корректности работы метода
+        assertThat(Calculator.calculatingDiscount(1234, 10)).isEqualTo(1110.60);
+        assertThat(Calculator.calculatingDiscount(1, 10)).isEqualTo(0.90);
+        assertThat(Calculator.calculatingDiscount(100, 99)).isEqualTo(1);
+        assertThat(Calculator.calculatingDiscount(100, 1)).isEqualTo(99);
+        //TODO: почему следующий тест не проходит? Ведь не должно быть исключения.
+//        assertThat(Calculator.calculatingDiscount(Double.MAX_VALUE-1, 50)).isEqualTo(234);
+
+        //Проверка вывода исключения при некорректном вводе суммы покупки
+        assertThatThrownBy(()->Calculator.calculatingDiscount(-122222223, 89))
+                .isInstanceOf(ArithmeticException.class);
+        assertThatThrownBy(()->Calculator.calculatingDiscount(0, 89))
+                .isInstanceOf(ArithmeticException.class);
+
+        //Проверка вывода исключения при некорректном вводе скидки
+        assertThatThrownBy(()-> Calculator.calculatingDiscount(12314, 123))
+                .isInstanceOf(ArithmeticException.class);
+        assertThatThrownBy(()->Calculator.calculatingDiscount(1, 0))
+                .isInstanceOf(ArithmeticException.class);
+    }
+}
