f37a23a752f40fcb7b585f8cee18f1ad5f3d8944
Zakhar <zox_13@mail.ru>
2024-04-02 06:31:21 +0300

    Abstract test class is fixed

--- AutoTestsHomework2/src/test/java/ComponentCircleTest.java
+++ AutoTestsHomework2/src/test/java/ComponentCircleTest.java
@@ -1,13 +1,20 @@
 import org.example.ComponentCircle;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ComponentCircleTest extends ComponentTest {
+public class ComponentCircleTest{
+    ComponentCircle circle1;
+    ComponentCircle circle2;
+    @BeforeEach
+    void circlesCreation(){
+        this.circle1 = new ComponentCircle();
+        this.circle2 = new ComponentCircle();
+    }
+
     @Test
-    void upsupportedTest(){
-        ComponentCircleTest componentCircleTest = new ComponentCircleTest();
-        ComponentCircle circle1 = new ComponentCircle();
-        ComponentCircle circle2 = new ComponentCircle();
-        componentCircleTest.unsupportedOperationsTest(circle1, circle2);
+    void unsupportedOperationsTest() {
+        Assertions.assertThrows(UnsupportedOperationException.class, ()->circle1.add(circle2));
+        Assertions.assertThrows(UnsupportedOperationException.class, ()->circle1.remove(circle2));
     }
 }
--- AutoTestsHomework2/src/test/java/ComponentSquareTest.java
+++ AutoTestsHomework2/src/test/java/ComponentSquareTest.java
@@ -1,13 +1,22 @@
 import org.example.ComponentSquare;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ComponentSquareTest extends ComponentTest{
+
+public class ComponentSquareTest{
+    ComponentSquare square1;
+    ComponentSquare square2;
+    @BeforeEach
+    void squareCreation(){
+        this.square1 = new ComponentSquare();
+        this.square2 = new ComponentSquare();
+    }
+
     @Test
-    void testTest(){
-        ComponentSquareTest componentSquareTest = new ComponentSquareTest();
-        ComponentSquare square1 = new ComponentSquare();
-        ComponentSquare square2 = new ComponentSquare();
-        componentSquareTest.unsupportedOperationsTest(square1, square2);
+    void unsupportedOperationsTest() {
+        Assertions.assertThrows(UnsupportedOperationException.class, ()->square1.add(square2));
+        Assertions.assertThrows(UnsupportedOperationException.class, ()->square1.remove(square2));
     }
 
 }
